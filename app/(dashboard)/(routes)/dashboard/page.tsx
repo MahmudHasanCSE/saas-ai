ea4b7ceccee99a20365832ead37348ed9ec3327d
Mahmud Hasan <rauaha@gmail.com>
2023-09-19 18:59:41 +0600

    feat: :sparkles: add sidebar and navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import { UserButton } from "@clerk/nextjs";
+// import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
     return (
         <div>
             <p className="text-6xl text-green-500 pb-5">Hello SaaS AI Dashboard Page(Protected)</p>
-            <UserButton afterSignOutUrl="/" />
+            {/* <UserButton afterSignOutUrl="/" /> */}
         </div>
     )
 }
